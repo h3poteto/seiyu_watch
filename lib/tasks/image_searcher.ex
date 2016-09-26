1cd49ce087a99a3567dae36c2699d8519ec95d10
h3poteto <h3.poteto@gmail.com>
2016-09-27 01:29:01 +0900

    refs #25 画像をローカルにダウンロードする

--- lib/tasks/image_searcher.ex
+++ lib/tasks/image_searcher.ex
@@ -3,6 +3,7 @@ defmodule SeiyuWatch.ImageSearcher do
   alias SeiyuWatch.Repo
   alias SeiyuWatch.GoogleResponse
 
+  require IEx
   def save_image(seiyu_id) do
     (SeiyuWatch.Seiyu
     |> Repo.get!(seiyu_id)
@@ -10,6 +11,33 @@ defmodule SeiyuWatch.ImageSearcher do
     |> google_search_request
     |> GoogleResponse.get_response
     |> GoogleResponse.parse_images
+    |> Enum.at(0)
+    |> download
+  end
+
+  # ダウンロードでつかえそうなもの
+  # http://qiita.com/mathhun/items/e78d779d2dca046a9dd8
+  # http://stackoverflow.com/questions/30267943/elixir-download-a-file-image-from-a-url
+
+  # 画像
+  # https://40.media.tumblr.com/dcfce2754e8c4d8917373f0a1e905686/tumblr_npjs1wR1UQ1ux6tqqo1_1280.jpg
+  def download(url) do
+    result = HTTPoison.get!(url)
+    case result do
+      %{status_code: 200, headers: headers, body: body} -> save(url, headers, body)
+      %{status_code: _code} -> :error
+    end
+  end
+
+
+  def save(url, headers, body) do
+    name = Crypto.md5(url)
+    file_name = case headers |> Enum.filter(fn(h) -> h |> elem(0) == "Content-Type" end) |> Enum.at(0) |> elem(1) do
+      "image/jpeg" -> "#{name}.jpg"
+      "image/gif" -> "#{name}.gif"
+      "image/png" -> "#{name}.png"
+    end
+    File.write!("/tmp/#{file_name}", body)
   end
 
   def google_search_request(name) do
