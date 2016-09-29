2d6f039ae34a17caed00093b8fdb65d0d836f89c
h3poteto <h3.poteto@gmail.com>
2016-09-30 00:22:44 +0900

    refs #41 diff計算前でも声優一覧画面がエラーにならないようにしておく

--- web/views/seiyu_view.ex
+++ web/views/seiyu_view.ex
@@ -1,3 +1,7 @@
 defmodule SeiyuWatch.SeiyuView do
   use SeiyuWatch.Web, :view
+
+  def recently_updated?(seiyu) do
+    seiyu.diffs_updated_at != nil && seiyu.diffs_updated_at |> Ecto.DateTime.to_erl |> Timex.to_datetime |> Timex.after?(Timex.shift(Timex.now, days: -7))
+  end
 end
