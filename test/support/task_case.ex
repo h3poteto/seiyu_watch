6e3205bd2bb15d1c5f444159d2019e2245eb713a
h3poteto <h3.poteto@gmail.com>
2016-10-04 00:23:54 +0900

    refs #45 Add task spec for wikipedia_response

---
+++ test/support/task_case.ex
@@ -0,0 +1,29 @@
+defmodule SeiyuWatch.TaskCase do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      alias SeiyuWatch.Repo
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+    end
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(SeiyuWatch.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(SeiyuWatch.Repo, {:shared, self()})
+    end
+
+    :ok
+  end
+
+  def errors_on(struct, data) do
+    struct.__struct__.changeset(struct, data)
+    |> Ecto.Changeset.traverse_errors(&SeiyuWatch.ErrorHelpers.translate_error/1)
+    |> Enum.flat_map(fn {key, errors} -> for msg <- errors, do: {key, msg} end)
+  end
+end
