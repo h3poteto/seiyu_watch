cb7bd5932dc5ea619a1b6c164145c0cd50a9d21d
h3poteto <h3.poteto@gmail.com>
2016-10-06 01:44:49 +0900

    refs #45 Add test for seiyu_parser

--- test/tasks/seiyu_parser_test.exs
+++ test/tasks/seiyu_parser_test.exs
@@ -8,7 +8,20 @@ defmodule SeiyuWatch.SeiyuParserTest do
   describe "#save" do
     test "should save seiyu" do
       with_mock SeiyuWatch.WikipediaResponse, [:passthrough], [get_response: fn(_request) -> response_category(%{page_id: 1543998, fullurl: "http://example.com", revision_id: 1, revision_hash: "hoge"}) end] do
-        assert SeiyuParser.save("阿澄佳奈") == {:ok, "阿澄佳奈"}
+        with_mock SeiyuWatch.SeiyuEvent, [after_create: fn(_create) -> :ok end] do
+          res = SeiyuParser.save("阿澄佳奈")
+          assert res != {:failed, "阿澄佳奈"}
+          assert Repo.get_by(SeiyuWatch.Seiyu, %{name: "阿澄佳奈"})
+        end
+      end
+    end
+
+    test "should call after_create event" do
+      with_mock SeiyuWatch.WikipediaResponse, [:passthrough], [get_response: fn(_request) -> response_category(%{page_id: 1543998, fullurl: "http://example.com", revision_id: 1, revision_hash: "hoge"}) end] do
+        with_mock SeiyuWatch.SeiyuEvent, [after_create: fn(_create) -> :ok end] do
+          {:ok, task} = SeiyuParser.save("阿澄佳奈")
+          assert Task.await(task) == :ok
+        end
       end
     end
   end
