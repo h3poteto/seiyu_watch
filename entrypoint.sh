6ddffaac502bc7c75289b05bf94c59618f8f5280
AkiraFukushima <h3.poteto@gmail.com>
2019-02-06 22:55:41 +0900

    Exit entrypoint when does not have aws credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 export AWS_DEFAULT_REGION=ap-northeast-1
+# Confirm aws credential to stop the container when the container is created.
+myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_host --region $AWS_DEFAULT_REGION
+ret=$?
+if [[ ret -ne 0 ]]; then
+    exit
+fi
+
 export DB_HOST=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_host --region $AWS_DEFAULT_REGION`
 export DB_USER=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_user --region $AWS_DEFAULT_REGION`
 export DB_PASSWORD=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_password --region $AWS_DEFAULT_REGION`
@@ -10,7 +17,5 @@ export GOOGLE_API_KEY=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.google_a
 export GOOGLE_CUSTOM_SEARCH_ID=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.google_custom_search_id --region $AWS_DEFAULT_REGION`
 export SLACK_WEBHOOK_URL=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.slack_webhook_url --region $AWS_DEFAULT_REGION`
 export RELX_REPLACE_OS_VARS=true
-ecs_erlang_cluster generate --cluster base-default-prd --service seiyuwatch-web-prd --region ap-northeast-1 --minport 4370 --maxport 4370
-export ONESELF=`ecs_erlang_cluster oneself`
 
 exec "$@"
