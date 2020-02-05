51af7a57c33673e3df9f22a88272efd7960d542f
AkiraFukushima <h3.poteto@gmail.com>
2020-02-05 22:06:51 +0900

    refs #185 Remove ssm in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-export AWS_DEFAULT_REGION=ap-northeast-1
-# Confirm aws credential to stop the container when the container is created.
-myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_host --region $AWS_DEFAULT_REGION
-ret=$?
-if [[ $ret -ne 0 ]]; then
-    exit
-fi
-
-export DB_HOST=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_host --region $AWS_DEFAULT_REGION`
-export DB_USER=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_user --region $AWS_DEFAULT_REGION`
-export DB_PASSWORD=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.db_password --region $AWS_DEFAULT_REGION`
-export SECRET_KEY_BASE=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.secret_key_base --region $AWS_DEFAULT_REGION`
-export S3_BUCKET=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.s3_bucket --region $AWS_DEFAULT_REGION`
-export GOOGLE_API_KEY=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.google_api_key --region $AWS_DEFAULT_REGION`
-export GOOGLE_CUSTOM_SEARCH_ID=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.google_custom_search_id --region $AWS_DEFAULT_REGION`
-export SLACK_WEBHOOK_URL=`myaws ssm parameter get seiyu_watch.$SERVICE_ENV.slack_webhook_url --region $AWS_DEFAULT_REGION`
-export RELX_REPLACE_OS_VARS=true
-
 exec "$@"
