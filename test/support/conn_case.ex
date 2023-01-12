7a3277925c749a5608954d69d9dac40d28bde10c
AkiraFukushima <h3.poteto@gmail.com>
2023-01-12 20:47:02 +0900

    Update tests

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -18,7 +18,8 @@ defmodule SeiyuWatchWeb.ConnCase do
   using do
     quote do
       # Import conveniences for testing with connections
-      use Phoenix.ConnTest
+      import Plug.Conn
+      import Phoenix.ConnTest
 
       alias SeiyuWatch.Repo
       import Ecto
