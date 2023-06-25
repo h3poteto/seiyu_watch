6b16a2c808d3b09c0a58480bbf8205231515276f
AkiraFukushima <h3.poteto@gmail.com>
2023-06-25 23:44:08 +0900

    Remove gettext

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule SeiyuWatch.Mixfile do
       version: "0.0.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
