4fe7b3d20e0c7883f7e213f00786e93194dfe0dc
AkiraFukushima <h3.poteto@gmail.com>
2023-06-26 22:45:07 +0900

    Use Config instead of Mix.Config

---
+++ config/runtime.exs
@@ -0,0 +1,24 @@
+import Config
+
+if config_env() == :prod do
+  config :seiyu_watch, SeiyuWatchWeb.Endpoint,
+    http: [port: String.to_integer(System.get_env("PORT") || "4000")],
+    secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+
+  config :rollbax,
+    access_token: System.fetch_env!("ROLLBAR_ACCESS_TOKEN"),
+    environment: "production",
+    enable_crash_reports: true,
+    enabled: true
+
+  # Configure your database
+  config :seiyu_watch, SeiyuWatch.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    username: System.fetch_env!("DB_USER"),
+    password: System.fetch_env!("DB_PASSWORD"),
+    database: "seiyu_watch",
+    hostname: System.fetch_env!("DB_HOST"),
+    show_sensitive_data_on_connection_error: true,
+    port: 5432,
+    pool_size: 3
+end
